cbb64c7b21658ec864a1fd2327691f7f944dc4e2
pierreashrafDE <pierreashraf001@gmail.com>
2026-05-06 20:07:42 +0300

    Update ddl_silver.sql

--- scripts/ddl_silver.sql
+++ scripts/ddl_silver.sql
@@ -15,7 +15,7 @@ CREATE TABLE silver.crm_cust_info
 	cst_key             NVARCHAR(50),
 	cst_firstname       NVARCHAR(50),
 	cst_lastname        NVARCHAR(50),
-	cst_material_status NVARCHAR(50),
+	cst_marital_status  NVARCHAR(50),
 	cst_gndr            NVARCHAR(50),
 	cst_create_date     DATE,
 	dwh_create_date     DATETIME2 DEFAULT GETDATE()
@@ -72,13 +72,13 @@ CREATE TABLE silver.erp_loc_a101
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
-IF OBJECT_ID('silver.erp_px_cat_gv2', 'U') IS NOT NULL
-	DROP TABLE silver.erp_px_cat_gv2;
-CREATE TABLE silver.erp_px_cat_gv2
+IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
+	DROP TABLE silver.erp_px_cat_g1v2;
+CREATE TABLE silver.erp_px_cat_g1v2
 (
 	id              NVARCHAR(50),
 	cat             NVARCHAR(50),
-	suncat          NVARCHAR(50),
+	subcat          NVARCHAR(50),
 	maintenance     NVARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
